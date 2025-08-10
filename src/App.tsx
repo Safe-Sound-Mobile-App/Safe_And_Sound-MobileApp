4d65fcd13ac3a917b13e3449570c337b2ab6cc05
TAGCH <pisit.au@ku.th>
2025-08-10 18:52:55 +0700

    css define

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import './style/app.css';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text className="text-green-500">Open up App.js to start working on your app!</Text>
+      <p>Welcome to the Safe and Sound mobile application!</p>
+      <title>Safe and Sound!</title>
+      <h1>Safe and Sound</h1>
+      <h2>Safe and Sound</h2>
+      <h3>Mobile App</h3>
+      <h4>Safe and Sound</h4>
+      <h5>Safe and Sound</h5>
       <StatusBar style="auto" />
     </View>
   );

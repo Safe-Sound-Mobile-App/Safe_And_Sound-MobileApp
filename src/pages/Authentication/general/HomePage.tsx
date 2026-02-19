0b5f7e8d0b6c9aa5c82b0c517eaab72738e022a6
TAGCH <pisit.au@ku.th>
2026-02-19 20:35:19 +0700

    add README.md

--- src/pages/Authentication/general/HomePage.tsx
+++ src/pages/Authentication/general/HomePage.tsx
@@ -55,59 +55,6 @@ export default function Home({ navigation }: Props) {
             <Text style={styles.signInButtonText}>SIGN IN</Text>
           </TouchableOpacity>
 
-          <View style={{ paddingTop: 16 }}>
-            <TouchableOpacity
-                style={[
-                  styles.signInButton,
-                  { backgroundColor: '#008080', paddingVertical: 12, paddingHorizontal: 16, borderRadius: 10 }
-                ]}
-                onPress={() => navigation.navigate('ElderInfoForm')}
-                activeOpacity={0.8}
-            >
-              <Text style={{ color: 'white', textAlign: 'center', fontWeight: '700' }}>Elder Form Page</Text>
-            </TouchableOpacity>
-          </View>
-
-            <View style={{ paddingTop: 16 }}>
-                <TouchableOpacity
-                    style={[
-                        styles.signInButton,
-                        { backgroundColor: '#008080', paddingVertical: 12, paddingHorizontal: 16, borderRadius: 10 }
-                    ]}
-                    onPress={() => navigation.navigate('CaregiverInfoForm')}
-                    activeOpacity={0.8}
-                >
-                    <Text style={{ color: 'white', textAlign: 'center', fontWeight: '700' }}>Caregiver Form Page</Text>
-                </TouchableOpacity>
-            </View>
-
-
-            <View style={{ paddingTop: 16 }}>
-                <TouchableOpacity
-                    style={[
-                        styles.signInButton,
-                        { backgroundColor: '#008080', paddingVertical: 12, paddingHorizontal: 16, borderRadius: 10 }
-                    ]}
-                    onPress={() => navigation.navigate('MainTabs')}
-                    activeOpacity={0.8}
-                >
-                    <Text style={{ color: 'white', textAlign: 'center', fontWeight: '700' }}>Caregiver HomePage</Text>
-                </TouchableOpacity>
-            </View>
-
-            <View style={{ paddingTop: 16 }}>
-                <TouchableOpacity
-                    style={[
-                        styles.signInButton,
-                        { backgroundColor: '#008080', paddingVertical: 12, paddingHorizontal: 16, borderRadius: 10 }
-                    ]}
-                    onPress={() => navigation.navigate('ElderMainTabs')}
-                    activeOpacity={0.8}
-                >
-                    <Text style={{ color: 'white', textAlign: 'center', fontWeight: '700' }}>Elder HomePage</Text>
-                </TouchableOpacity>
-            </View>
-
         </View>
       </View>
     </SafeAreaView>
